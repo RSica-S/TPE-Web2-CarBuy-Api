8b102627c9b369619cb6f88196da2b5d0857ed55
Ramiro Sica Suescun <ramirosicas@gmail.com>
2024-12-03 16:48:50 -0300

    se actualiza db

--- db/db_carbuy.sql
+++ db/db_carbuy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-11-2024 a las 23:53:39
+-- Tiempo de generación: 03-12-2024 a las 20:45:46
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -46,7 +46,14 @@ INSERT INTO `autos` (`id_auto`, `nombre_auto`, `descripcion`, `precio`, `id_marc
 (8, 'Frontier', 'Nissan Frontier Xgear 4x4 aut my25\r\n2024 | 0KM', '$46.600.000', 5),
 (9, 'Sentra', 'Nissan Sentra 2.0 Advance Cvt\r\n2022 | 25.000KM', '$38.500.800', 5),
 (10, 'Celica', 'Toyota Celica 1.6 | 1984 | 286.000KM', '$9.000.500', 1),
-(16, 'F40', 'FERRARI F40 2.0 Advance Cvt\r\n2022 | 25.000KM', '$380.500.800', 11);
+(16, 'F40', 'FERRARI F40 2.0 Advance Cvt\r\n2022 | 25.000KM', '$380.500.800', 11),
+(17, 'AE86', 'Toyota AE86 1.6 | 1984 | 286.000KM', '$9.000.500', 1),
+(18, 'Clio Mio', 'Renault Clio Mio 1.6 | 2004 | 286.000KM', '$6.000.500', 7),
+(22, 'Twingo', 'Twingo 1.6 | 2004 | 286.000KM', '$9.000.500', 7),
+(25, 'Focus', 'Focus 1.6 | 2004 | 286.000KM', '$9.000.500', 6),
+(26, '206', '206 1.8 | 2004 | 286.000KM', '$9.000.500', 8),
+(27, 'Cruze', 'Cruze 1.8 | 2014 | 86.000KM', '$9.000.500', 9),
+(28, 'Palio', 'Palio 1.4 | 208 | 186.000KM', '$9.000.500', 13);
 
 -- --------------------------------------------------------
 
@@ -68,12 +75,14 @@ INSERT INTO `marcas` (`id_marca`, `nombre_marca`, `img_marca`) VALUES
 (1, 'Toyota', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Toyota_logo_%28Red%29.svg/1200px-Toyota_logo_%28Red%29.svg.png'),
 (4, 'Mazda', 'https://upload.wikimedia.org/wikipedia/nah/b/b1/Mazda_logo.png?20080728005359'),
 (5, 'Nissan', 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Nissan_2020_logo.svg/1074px-Nissan_2020_logo.svg.png'),
-(6, 'Ford', ''),
-(7, 'Renault', ''),
-(8, 'Peugeot', ''),
-(9, 'Chevrolet', ''),
-(10, 'Fiat', ''),
-(11, 'Ferrari', 'logoferrari');
+(6, 'Ford', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Ford_Motor_Company_Logo.svg/768px-Ford_Motor_Company_Logo.svg.png'),
+(7, 'Renault', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Renault_2009_logo.svg/425px-Renault_2009_logo.svg.png'),
+(8, 'Peugeot', 'https://upload.wikimedia.org/wikipedia/commons/archive/f/f7/20240529142319%21Peugeot_Logo.svg'),
+(9, 'Chevrolet', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Chevrolet-logo.png/2560px-Chevrolet-logo.png'),
+(11, 'Ferrari', 'https://upload.wikimedia.org/wikipedia/sco/thumb/d/d1/Ferrari-Logo.svg/516px-Ferrari-Logo.svg.png?20150601144908'),
+(12, 'Bentley', 'https://upload.wikimedia.org/wikipedia/de/thumb/6/6c/Bentley_logo.svg/1200px-Bentley_logo.svg.png'),
+(13, 'Fiat', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/FIAT_logo_%282020%29.svg/280px-FIAT_logo_%282020%29.svg.png'),
+(14, 'Volkswagen', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Volkswagen_logo_2019.svg/900px-Volkswagen_logo_2019.svg.png');
 
 -- --------------------------------------------------------
 
@@ -129,13 +138,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `autos`
 --
 ALTER TABLE `autos`
-  MODIFY `id_auto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_auto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de la tabla `marcas`
 --
 ALTER TABLE `marcas`
-  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
